11a313d5341bb0b08ad946c18d0f2a55a186bc9d
Amir Kayumov <amir.kayumov7@gmail.com>
2025-10-21 11:04:27 -0400

    Updated fibonacci.py with functions
    Added user input validation and Fibonacci sequence generation functions.

--- fibonacci.py
+++ fibonacci.py
@@ -2,3 +2,35 @@
 
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
+
+def validate_user_input() -> int:
+    while (True):
+        inpt = input('How many terms of the sequence should be printed? ')
+        if (not inpt.isnumeric()):
+            print("Please enter a positive integer.")
+            continue
+        if (int(inpt) <= 0):
+            print("Please enter a positive integer.")
+            continue
+        return int(inpt)
+
+def fib(lst: []) -> int:
+    if len(lst) < 1:
+        return 0
+    elif len(lst) < 2:
+        return 1
+    else:
+        return lst[-2] + lst[-1]
+
+def print_sequence(num: int) -> None:
+    myList = []
+    for i in range(num):
+        myList.append(fib(myList))
+        print(myList[i])
+
+    
+def main():
+    num = validate_user_input()
+    print_sequence(num)
+
+main()
